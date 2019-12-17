a921709bdbd305a23373407bf3525226b0b0a51b
Betty42 <55029687+Betty42@users.noreply.github.com>
2019-12-17 15:58:27 +0200

    my own updates

--- age_to_bucket.py
+++ age_to_bucket.py
@@ -1,9 +1,17 @@
+'''
+This code recieves a json file which contains names paired with their ages, and the bucket ranges. 
+The output is a yaml file that parses the people to buckets according to their age range.
+To run this code for the sake of the test, kindly replace the json file path in the main function.
+'''
+
 import json
 
+# Load the json file to use its information
 def parse_json(json_file):
     with open(json_file, 'r') as json_data:
         return json.load(json_data)
 
+# Create bucktet ranges, create yaml file, call sort_and_append function    
 def bucket_parser(json_input):
     yaml_file = 'output.yaml'
     treshold = json_input['buckets']
@@ -16,6 +24,7 @@ def bucket_parser(json_input):
         max_range = treshold[-1]
         sort_and_append(min_range, max_range, ppl_ages, yaml_file)
 
+# Parse people into buckets and append to yaml
 def sort_and_append(min, max, tuples, output_file):
     ppl = []
     for person, age in tuples:
@@ -25,7 +34,8 @@ def sort_and_append(min, max, tuples, output_file):
     with open(output_file, 'a') as output:
         if ppl:
             output.write(str(min) + '-' + str(max) + ':\n    -' + '\n    -'.join(ppl) + '\n')
-
+            
+# Please replace 'hw.json' with your json path
 def main():
     json_input = parse_json('hw.json')
     bucket_parser(json_input)
