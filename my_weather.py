1607e44140a774f162f7953524f092d9ee7bbdd1
Betty42 <55029687+Betty42@users.noreply.github.com>
2019-12-21 22:12:28 +0200

    Create my_weather.py

---
+++ my_weather.py
@@ -0,0 +1,37 @@
+'''
+This script prints out the weather for the cities given as arguments.
+Cities should be separated by ','. FE: Dublin,Rome,London
+Optional: add a '-f' flag for Fahrenheit, '-c' flag for Celsius.
+If needed, please change the value of the api key.
+'''
+
+from requests import get
+import json
+import requests
+import sys
+
+# Change api key value to yours if needed
+api_key = 'bc30006e191a5efcc61d86b1db0153bd'
+
+def weather_in(city):
+    city_list = city[1].split(',')
+    fahrenheit = city[-1] == '-f'
+
+    for city in city_list:
+        try:
+            url = 'http://api.weatherstack.com/current?access_key={}&query={}'.format(api_key, city)
+            json_data = (requests.get(url)).json()
+
+            celsius = json_data['current']['temperature']
+            temperature = (celsius*1.8)+32 if fahrenheit else celsius
+            print('The weather in {} today is {} degrees'.format(city, temperature))
+
+        except KeyError as city_error:
+            print('{} does not exist, maybe try Tel-Aviv or Bacon'.format(city))
+
+
+def main(cities):
+    weather_in(cities)
+
+if __name__ == '__main__':
+    main(sys.argv)
