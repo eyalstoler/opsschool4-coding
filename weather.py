7736f3fc0aa2bae1ee93ad203d0795b4e28e38c9
Betty42 <55029687+Betty42@users.noreply.github.com>
2020-01-13 11:17:27 +0200

    pep8

--- weather.py
+++ weather.py
@@ -7,17 +7,20 @@ import sys
 @click.command()
 @click.option('--token', '-token', required=True, help='your API token')
 @click.option('--city', '-city', required=True, help='the cities separated by commas')
-@click.option('--T', 'temp_scale', '-T', type=click.Choice(['Celsius', 'Fahrenheit']), default='Celsius', show_default='Celsius', help='temperature scale')
+@click.option('--T', 'temp_scale', '-T', type=click.Choice(['Celsius', 'Fahrenheit']),
+                default='Celsius', show_default='Celsius', help='temperature scale')
 
 def weather(token, city, temp_scale):
     for city in city.split(','):
-        weather_response = get('http://api.weatherstack.com/current?access_key=%s&query=%s&units=%s' % (token, city, ('f' if temp_scale == 'Fahrenheit' else 'm')))
+        temp_url = 'f' if temp_scale == 'Fahrenheit' else 'm'
+        weather_response = get('http://api.weatherstack.com/current?access_key=%s&query=%s&units=%s' % (token, city, temp_url))
         city_json = json.loads(weather_response.text)
         try:
             city_temp = str(city_json['current']['temperature'])
-            click.echo('The weather in %s is %s degrees %s' % (city_json['location']['name'], city_temp, temp_scale))
+            location = city_json['location']['name']
+            click.echo('The weather in %s is %s degrees %s' % (location, city_temp, temp_scale))
         except (KeyError) as city_error:
-            print('Error: {} does not exist. Please try real cities like Blabla, Bigfoot, or Chicken'.format(city), file=sys.stderr)
+            print('Error: {} does not exist. Please try real cities like Blabla, Bigfoot, or Chicken'.format(city))
 
 if __name__ == '__main__':
     weather()
