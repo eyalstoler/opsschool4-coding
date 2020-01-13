434d264f4c75a72cb742f2bb2530898a364eb95f
Betty42 <55029687+Betty42@users.noreply.github.com>
2020-01-13 11:23:10 +0200

    %s to {}

--- weather.py
+++ weather.py
@@ -13,12 +13,12 @@ import sys
 def weather(token, city, temp_scale):
     for city in city.split(','):
         temp_url = 'f' if temp_scale == 'Fahrenheit' else 'm'
-        weather_response = get('http://api.weatherstack.com/current?access_key=%s&query=%s&units=%s' % (token, city, temp_url))
+        weather_response = get('http://api.weatherstack.com/current?access_key={}&query={}&units={}'.format(token,city,temp_url))
         city_json = json.loads(weather_response.text)
         try:
             city_temp = str(city_json['current']['temperature'])
             location = city_json['location']['name']
-            click.echo('The weather in %s is %s degrees %s' % (location, city_temp, temp_scale))
+            click.echo('The weather in %s is %s degrees %s' % (location,city_temp,temp_scale))
         except (KeyError) as city_error:
             print('Error: {} does not exist. Please try real cities like Blabla, Bigfoot, or Chicken'.format(city))
 
